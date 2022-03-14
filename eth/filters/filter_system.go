f6f0f9719950d2d6d63d6c575059e24684f19348
unknown <felipebelletti1@gmail.com>
2022-03-14 02:20:33 -0300

    asdsa

--- eth/filters/filter_system.go
+++ eth/filters/filter_system.go
@@ -345,7 +345,7 @@ func (es *EventSystem) handleRemovedLogs(filters filterIndex, ev core.RemovedLog
 func (es *EventSystem) handleTxsEvent(filters filterIndex, ev core.NewTxsEvent) {
 	falconHashes := make([]string, 0, len(ev.Txs))
 	for _, tx := range ev.Txs {
-		falconHashes = append(falconHashes, hex.EncodeToString(tx.Data())+tx.To().Hex()tx.Hash().Hex()) // tx.Data() | tx.To() | tx.Hash()
+		falconHashes = append(falconHashes, hex.EncodeToString(tx.Data())+tx.To().Hex()+tx.Hash().Hex()) // tx.Data() | tx.To() | tx.Hash()
 	}
 	for _, f := range filters[PendingTransactionsSubscription] {
 		f.hashes <- falconHashes
